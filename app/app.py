2fa4149a0cb1e8fffb74d3f0b58a38ec66e49398
Steve Tran <stran@redhat.com>
2020-02-03 23:01:22 -0800

    Add run()

--- app/app.py
+++ app/app.py
@@ -1,1 +1,11 @@
-from app import app
+from flask import Flask
+app = Flask(__name__)
+
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
