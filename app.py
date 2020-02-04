efb065579078a4421c1dc731cf673105a0c31674
Steve Tran <stran@redhat.com>
2020-02-04 12:18:37 -0800

    Add some more logic to it

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! - here only for testing\n'
+
+depmap = {}
+
+@app.route("/entry/<namespace>/<k8stype>/<name>", methods = ['POST', 'DELETE'])
+def registration(namespace, k8stype, name):
+    entries = depmap.get(namespace, [])
+    val = "%s/%s" % (k8stype, name)
+ 
+    if request.method == "POST":
+        if val not in entries:
+           print(val + " added\n")
+           entries.append(val)
+           depmap[namespace] = entries
+        else:
+           print(val + " already present\n")
+    elif request.method == "DELETE":
+       print(val + " removed\n")
+       entries.remove(val)
+
+    return "done\n"
+
+
+@app.route("/list")
+def listall():
+    return str(depmap)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
