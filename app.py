8e6ae0ffab9a8978e99592c1e09f842f22f24190
Steve Tran <stran@redhat.com>
2020-02-05 11:34:18 -0800

    Add even more logic to contract processing

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def hello_world():
     return 'Hello, World! - here only for testing\n'
 
 depmap = {}
+contracts = {}
 
 @app.route("/entry/<namespace>/<k8stype>/<name>", methods = ['POST', 'DELETE'])
 def registration(namespace, k8stype, name):
@@ -45,38 +46,61 @@ def list_projects():
 def dependencyCheck(namespace, k8stype, name):
   
    if dependencies_satisified(namespace, k8stype, name):
-       # add to deployed map
+       add_contracts(namespace, k8stype, name)
        return "Complete"
    else:
-       return "Waiting for %s" % (ret_val)
+       return "Incomplete"
        ## TODO this could return a smarter list of the individual pieces missing
 
 
-
+# 
 def dependencies_satisifed(namespace, k8stype, name):
-    #if deps is None or deps == "":
-    #    return true
-    
     satisifed = true
     deps = get_requires(namespace, k8stype, name)
+    env = get_env(namespace)
 
     if len(deps) > 0:
         for k in dep:
-        # check if the dependency is being served by someone in our map?
-        # TODO
-
+        # check if our contract dependency is available
+             if k not in contracts[env]:
+                 print("This service is missing a contract dependency on %s " % (k))
+                 satisified = false
+                 break
+    else:
+        print("No dependencies, we're good to start")
+ 
     return satisifed
 
+# Add provided contracts to our map
+def add_contracts(namespace, k8stype, name):
+    provs = get_provides(namespace, k8stype, name)
+    env = get_env(namespace)
+    for k in provs:
+        if contracts[env] is None:
+            contracts[env] = []
+        if k not in contracts[env]:
+            print("adding %s to our known contracts" % (k))
+            contracts[env].append(k)
 
+
+# TODO this should be safer
+def get_env(namespace):
+    return namespace.split("-")[1]
+
+
+# runs an OC command to grab a label
 def oc_get_labels_str(namespace, k8stype, name, label):
     cmd = "oc get %s/%s -n %s -o go-template='{{ index .metadata.labels \"%s\"}}'" % (k8stype, name, namespace, label)
     val = subprocess.check_output(cmd, shell=True).decode("utf-8")
     return val
 
+
+# returns a list of contracts being provided
 def get_provides(namespace, k8stype, name):
     return sanitize_list(oc_get_labels_str(namespace, k8stype, name, "gr.depman/provides"))
 
 
+# returns a list of contracts required
 def get_requires(namespace, k8stype, name):
     return sanitize_list(oc_get_labels_str(namespace, k8stype, name, "gr.depman/requires"))
 
@@ -88,7 +112,7 @@ def sanitize_list(my_str_list):
         sanitized = []
         for k in my_str_list.split(","):
             if k.strip() not in sanitized:
-                sanitized.add(k.strip())
+                sanitized.append(k.strip())
         return sanitized
 
 if __name__ == "__main__":
