40e3f09f4456d33e431e3f91bad36ded4ad8a534
Steve Tran <stran@redhat.com>
2020-02-05 10:03:31 -0800

    Add methods to clean up code

--- app.py
+++ app.py
@@ -42,19 +42,54 @@ def list_projects():
 
 
 @app.route("/dependencyCheck/<namespace>/<k8stype>/<name>")
-def check_dependencies(namespace, k8stype, name):
-   label = "gr.depman/requires"
-   cmd = "oc get %s/%s -n %s -o go-template='{{ index .metadata.labels \"%s\"}}'" % (k8stype, name, namespace, label)
+def dependencyCheck(namespace, k8stype, name):
+  
+   if dependencies_satisified(namespace, k8stype, name):
+       # add to deployed map
+       return "Complete"
+   else:
+       return "Waiting for %s" % (ret_val)
+       ## TODO this could return a smarter list of the individual pieces missing
 
-   ret_val = subprocess.check_output(cmd, shell=True)  # returns the exit code in unix
 
-   # split ret_val by comma delimiter,  trim pieces, compare with a map of provided dependencies
 
-   print('returned value:', ret_val)
-   print("dependencies = " + str(ret_val["gr.depman/requires"]))
+def dependencies_satisifed(namespace, k8stype, name):
+    #if deps is None or deps == "":
+    #    return true
+    
+    satisifed = true
+    deps = get_requires(namespace, k8stype, name)
 
-   return str(ret_val)
+    if len(deps) > 0:
+        for k in dep:
+        # check if the dependency is being served by someone in our map?
+        # TODO
 
+    return satisifed
+
+
+def oc_get_labels_str(namespace, k8stype, name, label):
+    cmd = "oc get %s/%s -n %s -o go-template='{{ index .metadata.labels \"%s\"}}'" % (k8stype, name, namespace, label)
+    val = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    return val
+
+def get_provides(namespace, k8stype, name):
+    return sanitize_list(oc_get_labels_str(namespace, k8stype, name, "gr.depman/provides"))
+
+
+def get_requires(namespace, k8stype, name):
+    return sanitize_list(oc_get_labels_str(namespace, k8stype, name, "gr.depman/requires"))
+
+
+def sanitize_list(my_str_list):
+    if my_str_list is None:
+        return []
+    else:
+        sanitized = []
+        for k in my_str_list.split(","):
+            if k.strip() not in sanitized:
+                sanitized.add(k.strip())
+        return sanitized
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
