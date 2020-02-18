36cc6c498b7c414fde42b335904e74f897fc406c
Steve Tran <stran@redhat.com>
2020-02-18 15:01:55 -0800

    Cleaned up dead methods, updated /contracts endpoint

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ db = apppersistence.Storage()
 def main_page():
     return render_template('main.html')
 
-# Contracts are stored as [<string>][List<contracts>] as environment (env) to contracts
-contracts = {}
+
 # Providers are stored as [<string>][[<string>][List<string>]] as namespace to k8s_objects to contracts
 providers = {}
 missing_dependencies = {}
@@ -53,37 +52,20 @@ def testu(id):
     db.update_controller(controller)
     return "done"
 
-## Testing only - delete when done
-@app.route("/test/<namespace>/<k8s>/<name>/<contract>", methods = ['POST', 'DELETE'])
-def test_method_only(namespace, k8s, name, contract):
-    env = get_env(namespace)
-    entries = contracts.get(env, [])
-
-    ns_to_type = providers.get(namespace, {})
-    provs = ns_to_type.get("%s/%s" % (k8s, name), [])
-
-    if request.method == "POST":
-        if contract not in entries:
-            entries.append(contract)
-            print(contract + " added\n")
-        if contract not in provs:
-            provs.append(contract)
-    elif request.method == "DELETE":
-       entries.remove(contract)
-       provs.remove(contract)
-       print(contract + " removed\n")
-
-    ns_to_type["%s/%s" % (k8s, name)] = provs
-
-    providers[namespace] = ns_to_type
-    contracts[env] = entries
-    return "done\n"
-
-
-# Contracts are stored as [<string>][List<contracts>] as environment (env) to contracts
+# Contracts are returned as a Map [<string>][List<contracts>] as environment (env) to contracts
 @app.route("/contracts")
-def list_contracts():
-    return json.dumps(db.select_contracts())
+def list_contracts_by_env():
+
+    results = {}
+    mapped_contracts = db.select_contracts()
+    # mapped_contracts is a map of [namespace][contracts]
+
+    for key in mapped_contracts:
+        c = results.get(get_env(key), [])
+        c.append(mapped_contracts[key])
+        results[get_env(key)] = c
+
+    return json.dumps(results)
 
 @app.route("/providers")
 def list_providers():
@@ -163,76 +145,10 @@ def register_service(namespace, manifest):
         return "Dependencies are missing", 418
 
 
-# TODO delete this, this isn't required
-#@app.route("/dependencyCheck/<namespace>/<k8stype>/<name>")
-def dependency_check(namespace, k8stype, name):
-
-   if dependencies_satisified(namespace, k8stype, name):
-       add_contracts(namespace, k8stype, name)
-       missing_dependencies.get(namespace, {}).pop("%s/%s" % (k8stype, name), [])
-       return "All good!", 200
-   else:
-       return "Dependencies are missing", 418
-       ## TODO this could return a smarter list of the individual pieces missing
-
-
 @app.route("/missing")
 def get_missing_dependencies():
-
-
     return json.dumps(missing_dependencies)
-#
-def dependencies_satisified(namespace, k8stype, name):
-    fulfilled = True
-    deps = get_requires(namespace, k8stype, name)
-    env = get_env(namespace)
 
-    # if we have more than 1 dependency, or if we only have 1 and it's not blank
-    if len(deps) > 1 or (len(deps) == 1 and deps[0].strip() != ""):
-        for k in deps:
-        # check if our contract dependency is available
-             if env not in contracts or k not in contracts[env]:
-                 print("This service is missing a contract dependency on %s " % (k))
-                 ns_to_type = missing_dependencies.get(namespace, {})
-                 missing = ns_to_type.get("%s/%s" %(k8stype, name), [])
-
-                 if k.strip() not in missing:
-                     missing.append(k.strip())
-                     ns_to_type["%s/%s" % (k8stype, name)] = missing
-                     missing_dependencies[namespace]  = ns_to_type
-
-                 fulfilled = False
-    else:
-        print("No dependencies, we're good to start")
-
-    return fulfilled
-
-
-#TODO delete this, OBE
-# Add provided contracts to our map
-def add_contracts(namespace, k8stype, name):
-    provs = get_provides(namespace, k8stype, name)
-    env = get_env(namespace)
-    for k in provs:
-        if env not in contracts:
-            contracts[env] = []
-        if k not in contracts[env]:
-            print("adding %s to our known contracts" % (k))
-            contracts[env].append(k)
-            add_provider(namespace, k8stype, name, k)
-
-# TODO delete this, OBE
-# Add contract provider to our map
-def add_provider(namespace, k8stype, name, contract):
-    key = "%s/%s" % (k8stype, name)
-    ns_to_type = providers.get(namespace, {})
-    contracts = ns_to_type.get(key, [])
-
-    if contract not in contracts:
-        contracts.append(contract)
-
-    ns_to_type[key] = contracts
-    providers[namespace] = ns_to_type
 
 # if namespace does not have a hypen in it, it just defaults to the name of the namespace
 def get_env(namespace):
